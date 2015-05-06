20dd2fe60c5fc551f755b741817c41b01a6e5fc4
Jeanella Klarys Pascual <jeanella@migo.tv>
2015-05-06 17:31:11 +0800

    Cleanup: Removed unused boilerplate code

--- js/app.js
+++ js/app.js
@@ -6,22 +6,13 @@ define([
     'regions/notification',
     'regions/dialog',
 	'collections/Nav',
-	'views/MenuView',
-	'views/Footer'
-], function (Backbone, Marionette, NotifyRegion, DialogRegion, Nav, MenuView, Footer) {
 	'use strict';
 
 	var app = new Marionette.Application();
 
-    app.pages = new Nav([
-        {title: 'Home', name: 'home', active: true},
-        {title: 'About', name: 'about'},
-        {title: 'Contact', name: 'contact'}
     ]);
-    var menu = new MenuView({collection: app.pages});
 
 	app.addRegions({
-		menu: '#main-nav',
 		main: '#main',
 		footer: '#footer',
         notification: {
@@ -35,7 +26,6 @@ define([
 	});
 
 	app.addInitializer(function () {
-        app.menu.show(menu);
 		app.footer.show(new Footer());
 	});
 
@@ -45,12 +35,6 @@ define([
         }
     });
 
-	app.vent.on('menu:activate', function (activePageModel) {
-        menu.collection.findWhere({active: true})
-            .set('active', false);
-        activePageModel.set('active', true);
-        menu.render();
-	});
 
     /**
      * Sample JSON Data
