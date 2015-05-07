205b4f61aa57241258a9117054591df9ea23d2c7
Jeanella Klarys Pascual <jeanella@migo.tv>
2015-05-08 00:47:37 +0800

    WeatherUtility: Removed timeOfDay for better icon support for Daylight Savings

--- js/utility/weatherUtility.js
+++ js/utility/weatherUtility.js
@@ -21,17 +21,11 @@ define([
         {name: 'clear-day', timeOfDay: 'day', icon: 'wi-day-sunny'},
         {name: 'clear-night', timeOfDay: 'night', icon: 'wi-night-clear'},
         {name: 'rain', timeOfDay: 'day', icon: 'wi-day-rain'},
-        {name: 'rain', timeOfDay: 'night', icon: 'wi-night-rain'},
         {name: 'snow', timeOfDay: 'day', icon: 'wi-day-snow'},
-        {name: 'snow', timeOfDay: 'night', icon: 'wi-night-snow'},
         {name: 'sleet', timeOfDay: 'day', icon: 'wi-day-snow'},
-        {name: 'sleet', timeOfDay: 'night', icon: 'wi-night-snow'},
         {name: 'wind', timeOfDay: 'day', icon: 'wi-day-windy'},
-        {name: 'wind', timeOfDay: 'night', icon: 'wi-night-alt-cloudy-windy'},
         {name: 'fog', timeOfDay: 'day', icon: 'wi-day-fog'},
-        {name: 'fog', timeOfDay: 'night', icon: 'wi-night-fog'},
         {name: 'cloudy', timeOfDay: 'day', icon: 'wi-day-cloudy'},
-        {name: 'cloudy', timeOfDay: 'night', icon: 'wi-night-alt-cloudy'},
         {name: 'partly-cloudy-day', timeOfDay: 'day', icon: 'wi-day-sunny-overcast'},
         {name: 'partly-cloudy-night', timeOfDay: 'night', icon: 'night-partly-cloudy'}
     ];
@@ -46,7 +40,6 @@ define([
          */
         getIconClass: function (weatherName, time, timezone){
             return _.findWhere(forecastIconSet, {
-                timeOfDay: this.getTimeOfDay(time, timezone),
                 name: weatherName
             }).icon;
         },
